23d8f1a1d7baeae35b6afeaa5e4c666004f026f4
starryowl <henry7007@khu.ac.kr>
2024-04-02 02:28:03 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.utils import timezone       # 게시글을 게시일로 정렬할려
 from django.shortcuts import render, get_object_or_404    # 뷰 추가
 
 from .models import Post                # .은 현재 디렉토리
-
-
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):                # view function은 항상 첫번째 인자로 request를 갖는다
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -14,3 +14,30 @@ def post_list(request):                # view function은 항상 첫번째 인�
 def post_detail(request, pk):         # view 새부 페이지
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
